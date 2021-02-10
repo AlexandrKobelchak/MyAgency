5a9bd51de7e858fc38196fde1fd1e6245636bf22
Alexandr Kobelchak <kobelchak@gmail.com>
2021-02-10 20:35:28 +0200

    added generic service

---
+++ Repositories/Application/SocialNetworkRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    public class SocialNetworkRepository : DbRepository<SocialNetwork>, ISocialNetworkRepository
+    {
+        public SocialNetworkRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
